9f1f4e2812fb5ec1802c0503466f5879b1dfb232
markna551 <auttakrit.w@ku.th>
2023-04-14 16:44:15 +0700

    enhance get total amount check digit of amount

--- routes/contracts.js
+++ routes/contracts.js
@@ -5,9 +5,17 @@ const contract = require('../contracts/conect_contract')
 
 router.get('/getAmount/:id', async (req, res) => {
     try {
+      let result;
       const address = req.params.id;
       const value = await contract.methods.balanceOf(address).call();
-      const result = Web3.utils.fromWei(value, 'ether');
+      const totalEther = Web3.utils.fromWei(value, 'ether');
+      const amount = parseFloat(totalEther);
+      if (amount % 1 === 0) {
+        result = amount
+      }
+      else {
+        result = parseFloat(amount).toFixed(2)
+      }
       res.send({ result });
     } catch (error) {
       res.status(500).send({ error: error.message });
